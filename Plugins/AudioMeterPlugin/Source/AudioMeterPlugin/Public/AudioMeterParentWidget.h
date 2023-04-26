7c00cb560fdf2ee3484e00b7cffd2da1c9746934
Curtis Smith <curtismith04@gmail.com>
2023-04-26 14:02:05 -0700

    Working on bird chirps for the creatures

---
+++ Plugins/AudioMeterPlugin/Source/AudioMeterPlugin/Public/AudioMeterParentWidget.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blutility/Classes/EditorUtilityWidget.h"
+#include "AudioMeterParentWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AUDIOMETERPLUGIN_API UAudioMeterParentWidget : public UEditorUtilityWidget
+{
+	GENERATED_BODY()
+};
